06a75ab3a07ec6901001161ec5679f328526f372
Joe Butler <joefish.b@gmail.com>
2020-06-25 18:03:02 +1200

    Create basic deck component, not doing any API calls yet

---
+++ client/components/Deck.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import request from 'superagent'
+import { render } from 'enzyme'
+
+class Deck extends React.Component {
+  constructor(props) {
+    super(props)
+    state = {
+      deckId: ''
+    }
+  }
+}
+
+// functions to get a deck
+
+  render() {
+    return (
+      <div>
+        <Card />
+      </div>
+    )
+  }
+
+export default Deck
